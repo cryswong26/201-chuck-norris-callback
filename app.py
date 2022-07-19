9728ca6a3c86bab84db4a59037ade46537ea9cc6
Crystal <cryswong26@gmail.com>
2022-07-19 22:56:51 +0000

    I changed the python file

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import dash_html_components as html
 import os
 
 ###### Set up variables
-list_of_choices=['punch', 'body-slam', 'round-house kick to the face']
-githublink = 'https://github.com/austinlasseter/chuck_norris_execution'
-image1='chucknorris.jpg'
-heading1='Chuck Norris execution method'
+list_of_choices=['squat', 'bench press', 'pull up']
+githublink = 'https://github.com/cryswong26/201-chuck-norris-callback'
+image1='Orion Gym.jpg'
+heading1='My Favorite Exercises'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -22,7 +22,7 @@ app.layout = html.Div([
     html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
     dcc.Dropdown(id='your-input-here',
                 options=[{'label': i, 'value': i} for i in list_of_choices],
-                value='punch',
+                value='squat',
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -36,7 +36,7 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return f'Chuck Norris will now execute you with a {whatever_you_chose}.'
+    return f'I will now show you a {whatever_you_chose}.'
 
 
 ######### Run the app #########
